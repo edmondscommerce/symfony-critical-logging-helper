72c85752a5469227b9d42c2a2f6055a8ee0338a6
ben <ben@ben>
2019-07-09 09:22:01 +0000

    Add additional context

--- src/CriticalLogger.php
+++ src/CriticalLogger.php
@@ -3,25 +3,43 @@
 namespace EdmondsCommerce\CriticalLogger;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CriticalLogger
 {
+    /**
+     * @var LoggerInterface
+     */
     private $logger;
+    /**
+     * @var string
+     */
+    private $uri;
+    /**
+     * @var string
+     */
+    private $host;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, RequestStack $requestStack)
     {
         $this->logger = $logger;
+
+        $request    = $requestStack->getCurrentRequest();
+        $this->uri  = $request->getRequestUri();
+        $this->host = $request->getHttpHost();
     }
 
     public function log(string $message, \Throwable $throwable, array $context = []): void
     {
+        $type = get_class($throwable);
         $file = $throwable->getFile();
         $line = $throwable->getLine();
 
-        $throwableDetails = "[file:$file][line:$line]";
+        $throwableDetails = "[host:{$this->host}][uri:{$this->uri}][type:$type][file:$file][line:$line] ";
 
         $this->logger->critical(
-            $message . $throwableDetails,
+            $throwableDetails . $message,
             $context
         );
     }
